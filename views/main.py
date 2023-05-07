f74247af2383f3dcead63070f11bf65f4af5bb91
web-mc <peshkov.mc@gmail.com>
2023-05-07 12:44:20 +0300

    22

--- views/main.py
+++ views/main.py
@@ -4,7 +4,7 @@ from app import app
 
 @app.get("/")
 async def main_page():
-    return {"message": "Hello55 World"}
+    return {"message": "Hello5555 World"}
 
 # @app.get("/items/{item_id}")
 # async def read_item(item_id: int):
